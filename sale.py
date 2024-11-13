fa2b18b458bf5d6bfc187b02851eb12e408efe19
WHA100 <135410611+WHA100@users.noreply.github.com>
2024-11-13 17:53:54 +0300

    Update sale.py

--- sale.py
+++ sale.py
@@ -117,4 +117,4 @@ if __name__ == "__main__":
     import uvicorn
     load_chat_history()
     asyncio.run(main())
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
